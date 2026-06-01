1b7feef5ee6d84050c03af8c446c1ff72ce028d2
SayyidIlmi <sayyidilmihubballillah@gmail.com>
2026-06-01 15:44:36 +0700

    Cleanup vercel

--- config/database.php
+++ config/database.php
@@ -147,17 +147,12 @@ return [
 
         'client' => env('REDIS_CLIENT', 'phpredis'),
 
-        // 'options' => [
-        //     'cluster' => env('REDIS_CLUSTER', 'redis'),
-        //     'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
-        //     'persistent' => env('REDIS_PERSISTENT', false),
-        // ],
+        'options' => [
+            'cluster' => env('REDIS_CLUSTER', 'redis'),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
+            'persistent' => env('REDIS_PERSISTENT', false),
+        ],
 
-        'options' => extension_loaded('pdo_mysql') ? array_filter([
-            PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', true),
-            // 💡 Baris di bawah ini memaksa Laravel mengizinkan koneksi SSL tanpa verifikasi sertifikat lokal yang ribet
-            PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-        ]) : [],
 
         'default' => [
             'url' => env('REDIS_URL'),
